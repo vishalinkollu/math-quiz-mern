964a15fecb6ecdcbe9592665b0795d1a9a251c47
vishalinkollu <vishalinkollu2002@gmail.com>
2026-04-10 22:36:50 +0530

    updated backend URL

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
+const socket = io("https://math-quiz-mern.onrender.com", {
   reconnection: true,
   reconnectionAttempts: 5,
   reconnectionDelay: 1000,
